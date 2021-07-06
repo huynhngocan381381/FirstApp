208179a51fc913fc81786ca8151b037a30f8e008
Huỳnh Ngọc An <huynhngocan381381@gmail.com>
2021-07-06 15:18:38 +0700

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Van Ha on 7/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ByeView()
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Van Ha on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
